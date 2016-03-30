83c3cf13a29f95e7158f0de1d27e22442e869856
Piotr Kedra <piotr.kedra@schibsted.pl>
2016-03-30 14:29:29 +0200

    Fix path for phpunit

--- bin/tasks/run-phpunit-tests.sh
+++ bin/tasks/run-phpunit-tests.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_TEST_RESULT" = "1" ]; then
 fi
 # add code BELOW this line
 
-PHPUNIT_DIR="$TRAVIS_BUILD_DIR/vendor/phpunit/phpunit"
+PHPUNIT_DIR="$TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/composer/bin/phpunit"
 
 
 $PHPUNIT_DIR/phpunit --testdox --bootstrap vendor/autoload.php app/
