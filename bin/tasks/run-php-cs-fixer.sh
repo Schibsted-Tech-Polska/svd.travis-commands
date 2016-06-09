b32f8a490335bbe6d4a50d56580a6a66a939d384
Piotr Kedra <piotr.kedra@schibsted.pl>
2016-06-09 11:37:06 +0200

    Fix path for php-cs-fixer

--- bin/tasks/run-php-cs-fixer.sh
+++ bin/tasks/run-php-cs-fixer.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_TEST_RESULT" = "1" ]; then
 fi
 # add code BELOW this line
 
-PHPCSFIXER_DIR="$TRAVIS_BUILD_DIR/vendor/fabpot/php-cs-fixer"
+PHPCSFIXER_DIR="$TRAVIS_BUILD_DIR/vendor/friendsofphp/php-cs-fixer"
 
 
 $PHPCSFIXER_DIR/php-cs-fixer --dry-run --verbose fix
