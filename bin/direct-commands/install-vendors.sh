72263ff42167788e3b84977b45ed57afbf728730
Piotr Kedra <piotr.kedra@schibsted.pl>
2016-10-24 15:30:45 +0200

    Small fix

--- bin/direct-commands/install-vendors.sh
+++ bin/direct-commands/install-vendors.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-composer config github-oauth.github.com $GITHUB_AUTH_TOKEN
+composer config github-oauth.github.com "$GITHUB_AUTH_TOKEN"
 composer install --ignore-platform-reqs --no-interaction --prefer-dist
