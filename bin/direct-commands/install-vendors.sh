c76d637a3552302d887e50b79789e6d1f9b04f79
Piotr Kedra <piotr.kedra@schibsted.pl>
2017-01-27 14:12:01 +0100

    Fix problem with outdated composer

--- bin/direct-commands/install-vendors.sh
+++ bin/direct-commands/install-vendors.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
+composer selfupdate
 composer config github-oauth.github.com "$GITHUB_AUTH_TOKEN"
 composer install --ignore-platform-reqs --no-interaction --prefer-dist
